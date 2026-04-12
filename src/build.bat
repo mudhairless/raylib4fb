6ab6dda12a7bf8463a025ed8bba27a9d897df242
Ebben Feagan <ebben.feagan@gmail.com>
2026-04-12 16:08:37 -0500

    build.bat in src dir will move output files into their destination

--- src/build.bat
+++ src/build.bat
@@ -1,13 +1,17 @@
 gcc -m32 -c raygui.c
-fbc32 -lib raygui.o -x libraygui.32.a
+fbc32 -lib raygui.o -x libraygui.a
+move libraygui.a ..\lib\win32
 
 gcc -c raygui.c
-fbc64 -lib raygui.o -x libraygui.64.a
+fbc64 -lib raygui.o -x libraygui.a
+move libraygui.a ..\lib\win64
 
 gcc -m32 -c rlgl.c
-fbc32 -lib rlgl.o -x librlgl.32.a
+fbc32 -lib rlgl.o -x librlgl.a
+move librlgl.a ..\lib\win32
 
 gcc -c rlgl.c
-fbc64 -lib rlgl.o -x librlgl.64.a
+fbc64 -lib rlgl.o -x librlgl.a
+move librlgl.a ..\lib\win64
 
-echo RayGui and RLGL are built, copy the static libs to the appropriate directory and remove the bit indicator from the extensions
+@echo RayGui and RLGL are built!
